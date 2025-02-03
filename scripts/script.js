b56e8883025f4e8091e1ba21da5f4c1671d8ef8a
hopaPlock <123625663+hopaPlock@users.noreply.github.com>
2025-02-03 14:10:09 +0100

    Added Pokémon randomizer

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const log = (msg) => console.log(msg);
 
 // I denna fil skriver ni all er kod
+let activePokemons = []
 
+//Form validation
 let formRef = document.querySelector('#form');
 formRef.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -11,6 +13,7 @@ formRef.addEventListener('submit', (event) => {
     }
 })
 
+// Form validation function
 function validateLogin() {
     let nickRef = document.querySelector('#nick').value;
     let ageRef = document.querySelector('#age').value;
@@ -35,3 +38,48 @@ function validateLogin() {
     return true
 }
 
+// Slumpa 10 random pokémon
+function getPokemons() {
+
+    let pokemonNumbers = []
+
+    for (let i = 1; i <= 151; i++) {
+        pokemonNumbers.push(i.toString()) 
+    }
+    
+    pokemonNumbers = pokemonNumbers.map((number) => {
+        if (number.length === 1) {
+            return `00${number}`
+        } else if (number.length === 2){
+            return `0${number}`
+        } else {
+            return number
+        }
+    })
+
+    shuffleArray(pokemonNumbers)
+
+    let randomPokemonNumbers = pokemonNumbers.slice(0, 10)
+
+    console.log(randomPokemonNumbers);
+
+    activePokemons = randomPokemonNumbers.map(number => ({
+        idCss: `poke${number}`,
+        imageUrl: `/assets/pokemons/${number.toString()}.png`,
+        ballImg: `/assets/ball.webp`
+    }))
+
+    console.log(activePokemons);
+    
+}
+
+// Funktion för att blanda en array (Fisher-Yates shuffle)
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+
+getPokemons()
